bcc585f16629d6bc8722f6ce517cff44d98559c8
Pankaj K S <SPankaj@ESTUATE.COM>
2018-01-19 18:37:52 +0530

    Added the public folder

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 var app = express();
+
+app.use(express.static(__dirname+'/public'));
 
 app.get('/', (req, res) => {
 	//res.send('<h3> Hello Pankaj </h3>');
@@ -11,6 +12,15 @@ app.get('/', (req, res) => {
 	});	
 });
 
+app.get('/error', (req, res) => {
+	res.send('<h3 style="color:red;"> Error Page </h3>');	
+});
 
-app.listen(3000);
+app.get('/about', (req, res) => {
+	res.send('<h3> About Page </h3>');	
+});
+
+app.listen(3000, () => {
+	console.log('Server is up port 3000 !!!');
+});
 
